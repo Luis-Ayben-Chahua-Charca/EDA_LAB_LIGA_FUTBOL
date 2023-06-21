66806b180ba4942e6cdc4ad8454efa1925d83d38
Chambi Orosco Luis Fernando <lchambio@unsa.edu.pe>
2023-06-20 23:50:20 -0500

    modificar

--- BD/ConexionBD/src/Conexion.java
+++ BD/ConexionBD/src/Conexion.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 public class Conexion {
+	
 	private static final String CONTROLADOR="com.mysql.cj.jdbc.Driver";
 	private static final String URL="jdbc:mysql://localhost:3306/liga_futbol";
 	private static final String USUARIO="root";
@@ -23,28 +24,33 @@ public class Conexion {
 		return conexion;
 	}
 	public static void main(String[] args) {
-		Conexion c= new Conexion();
+		Conexion c1= new Conexion();
 		Connection cn=null;
 		Statement stm=null;
 		ResultSet rs=null;
+               
 		try{
-			cn=c.conectar();
+			cn=c1.conectar();
 			stm=cn.createStatement();
-			rs=stm.executeQuery("SELECT * FROM fixture_det");
+			rs=stm.executeQuery("SELECT * FROM arbitros");
 			while(rs.next()){
 				int id=rs.getInt(1);
-				int a=rs.getInt(2);
-				int b=rs.getInt(3);
-				String d=rs.getString(4);
-				System.out.println(id+" "+a+" "+b+" "+d+" ");
+				String a=rs.getString(2);
+				String b=rs.getString(3);
+				int  c=rs.getInt(4);
+				String d=rs.getString(5);
+				String e=rs.getString(6);
+
+				
+				System.out.println("IDarbitro:"+id+" Nombre:"+a+" Categoria:"+b+" DNI:"+c+" edad:"+d+" Estado de registro:"+e);
 
 			}
 			
 		}catch(SQLException e){
-
+			System.out.println("error");
 		}
 
-
 	}
+	
 
 }
